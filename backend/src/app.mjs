e43f0b2adeb7f1d0cfdfd73d2c0eb8845dcc6026
e.covello <emanuele.covello@gmail.com>
2020-10-05 17:04:00 +0200

    backend/express enable cors

--- backend/src/app.mjs
+++ backend/src/app.mjs
@@ -1,8 +1,9 @@
 import express from 'express';
 import * as config from './config';
+import cors from 'cors';
 
 const app = express();
-
+app.use(cors());
 app.listen(config.PORT);
 
 app.get('/api/orders', (req, res) => {
