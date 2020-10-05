1c1e368016f2ab66f31b43ef8d8eddbd98715fc0
e.covello <emanuele.covello@gmail.com>
2020-10-05 15:38:55 +0200

    views/main:  basic ui with navigation to contact-for and orders

--- frontend/src/views/Main.vue
+++ frontend/src/views/Main.vue
@@ -1,3 +1,37 @@
 <template>
-  <div>MAIN VIEW</div>
+  <div :class="$style.Main">
+    <h1 :class="$style.PageTitle">Help Center</h1>
+    <input :class="$style.SearchBar" type="search" placeholder="search" />
+    <div :class="$style.Nav">
+      <router-link tag="div" :to="{ name: 'contactForm' }">
+        <button>Contact Form</button>
+      </router-link>
+      <router-link tag="div" :to="{ name: 'orders' }">
+        <button>
+          Where is my order?
+        </button>
+      </router-link>
+    </div>
+  </div>
 </template>
+
+<style module lang="scss">
+.Main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .PageTitle {
+    text-align: center;
+    margin: $spacing-xlarge 0;
+  }
+
+  .SearchBar {
+    min-width: 400px;
+  }
+
+  .Nav {
+    display: flex;
+  }
+}
+</style>
